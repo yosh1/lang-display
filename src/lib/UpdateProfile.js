168d4b1d3f16433347db9431723f5948861f63aa
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-12-18 20:35:39 +0900

    [Add] Update profile

--- src/lib/UpdateProfile.js
+++ src/lib/UpdateProfile.js
@@ -1,5 +1,18 @@
 require("dotenv").config();
-import axios from "axios";
+import twit from "twit";
+
+const Twit = require('twit');
+
+const T = new Twit({
+  consumer_key: '...',
+  consumer_secret: '...',
+  access_token: '...',
+  access_token_secret: '...',
+  timeout_ms: 60*1000,  
+  strictSSL: true,  
+});
+
+T.post('account/update_profile', { name: displayName }, (err, data, res) => {});
 
 module.exports = displayName => {
 
