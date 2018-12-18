ce7858cc2d34cc1d0f51250db2bd9951aa891abb
OldBigBuddha <shinlag@outlook.jp>
2018-12-18 22:18:27 +0200

    [add] comment

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import updateProfile from './lib/UpdateProfile.js'
 
 // cron.schedule("* * * * * *", () => {
 //     console.log("Hello World");
-// });
 
+// GitHub APIから言語を取得
+
+// 取得した言語を渡す
 updateProfile('OBB a.k.a OJI')
+// });
