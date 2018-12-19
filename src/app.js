2f4fe458055c343750c76fcbc3d43528cd824ba6
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-12-19 14:20:54 +0900

    [Update] GitHub API

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import cron from "node-cron";
 import updateProfile from './lib/UpdateProfile.js'
+import githubApi from './lib/GitHubApi.js'
 
 // cron.schedule("* * * * * *", () => {
 //     console.log("Hello World");
 
 // GitHub APIから言語を取得
-
+githubApi('yoshi1125hisa')
 // 取得した言語を渡す
-updateProfile('OBB a.k.a OJIs')
+// updateProfile('yoshi1125hisa')
 // });
