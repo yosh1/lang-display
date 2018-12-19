eac60ed3c740b919b9929b0e4d027986da81850e
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-12-19 14:22:34 +0900

    [Update] Github api

--- src/app.js
+++ src/app.js
@@ -6,15 +6,8 @@ import githubApi from './lib/GitHubApi.js'
 //     console.log("Hello World");
 
 // GitHub APIから言語を取得
-<<<<<<< HEAD
 githubApi('yoshi1125hisa')
 // 取得した言語を渡す
 // updateProfile('yoshi1125hisa')
-=======
-githubApi('OldBigBuddha')
 
-// 取得した言語を渡す
-// updateProfile('OBB a.k.a OJI')
-
->>>>>>> 7371d352f059211959ba01177d77963d7676f6cd
 // });
